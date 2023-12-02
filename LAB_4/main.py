46521f9edf73e1aaef802d320b9a5738ca8111c8
s22020 <s22020@pjwstk.edu.pl>
2023-12-02 22:51:49 +0100

    Add executing Selenium test scenarios

--- LAB_4/main.py
+++ LAB_4/main.py
@@ -1,9 +1,16 @@
 from chrome_wow import ChromeWow
 from firefox_wow import FirefoxWow
 from chrome_tawerna import ChromeTawerna
+from firefox_tawerna import FirefoxTawerna
 
 chrome_wow = ChromeWow()
 chrome_wow.run_add_product_to_cart_and_ship_test()
 
 chrome_tawerna = ChromeTawerna()
 chrome_tawerna.run_failed_login_flow()
+
+firefox_wow = FirefoxWow()
+firefox_wow.run_add_product_to_cart_and_ship_test()
+
+firefox_tawerna = FirefoxTawerna()
+firefox_tawerna.run_failed_login_flow()
