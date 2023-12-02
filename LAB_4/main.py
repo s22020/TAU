6300cb1d948246be9ae950bbe45c087d90d87bfd
s22020 <s22020@pjwstk.edu.pl>
2023-12-02 20:55:26 +0100

    Add Tawerna RPG forum failed login test

--- LAB_4/main.py
+++ LAB_4/main.py
@@ -1,5 +1,9 @@
 from chrome_wow import ChromeWow
+from firefox_wow import FirefoxWow
+from chrome_tawerna import ChromeTawerna
 
 chrome_wow = ChromeWow()
-
 chrome_wow.run_add_product_to_cart_and_ship_test()
+
+chrome_tawerna = ChromeTawerna()
+chrome_tawerna.run_failed_login_flow()
