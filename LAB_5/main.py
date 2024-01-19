fcfecc195a5e45ad927231f89b14b355bcc2b42a
s22020 <s22020@pjwstk.edu.pl>
2024-01-19 17:21:54 +0100

    Add methods for setting initial positions of player, finish and obstacles on the board

--- LAB_5/main.py
+++ LAB_5/main.py
@@ -1,3 +1,5 @@
+import random
+
 class BoardGame:
     '''
      | | | | 
@@ -15,12 +17,44 @@ class BoardGame:
         self.player_position = None
         # create obstacles set
         self.obstacles = set()
+        self._generate_positions()
+
+    def _generate_edge_coords(self):
+        side = random.choice(['top', 'right', 'left', 'bottom'])
+        if side == 'top':
+            return 0, random.randint(0, self.cols - 1)
+        elif side == 'bottom':
+            return self.rows - 1, random.randint(0, self.cols - 1)
+        elif side == 'left':
+            return random.randint(0, self.rows - 1), 0
+        elif side == 'right':
+            return random.randint(0, self.rows - 1), self.cols - 1
+
+    def _generate_obstacle_coords(self):
+        return random.randint(0, self.rows - 1), random.randint(0, self.cols - 1)
+
+    def _generate_positions(self):
+        self.player_position = self._generate_edge_coords()
+        self.finish = self._generate_edge_coords()
+
+        while self.finish == self.player_position:
+            self.finish = self._generate_edge_coords()
+
+        for i in range(min(self.rows * self.cols // 5, 10)):
+            obstacle = self._generate_obstacle_coords()
+
+            while obstacle == self.finish or obstacle == self.player_position or obstacle in self.obstacles:
+                obstacle = self._generate_obstacle_coords()
+            
+            self.obstacles.add(obstacle)
 
     def print_board(self):
         print("- " * 5)
         for row in self.board:
             print('|'.join(row))
         print()
+
+
 
 board_game = BoardGame(5, 5)
 board_game.print_board()
