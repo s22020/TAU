0bea4c5dfdefc194648ca48ad225e9e1d9ce1a9a
s22020 <s22020@pjwstk.edu.pl>
2024-01-20 21:08:28 +0100

    Add test methods for updating board and invalid move

--- LAB_5/unit_tests.py
+++ LAB_5/unit_tests.py
@@ -1,5 +1,7 @@
 import unittest
 from main import BoardGame
+from unittest.mock import patch
+from io import StringIO
 
 unittest.TestLoader.sortTestMethodsUsing = None
 
@@ -27,7 +29,6 @@ class TestBoardGame(unittest.TestCase):
         self.assertTrue(0 <= obstacle_coords[0] < self.board_game.rows)
         self.assertTrue(0 <= obstacle_coords[1] < self.board_game.cols)
 
-
     def test_should_generate_positions_properly(self):
         self.assertIsNotNone(self.board_game.player_position)
         self.assertIsNotNone(self.board_game.finish)
@@ -36,6 +37,24 @@ class TestBoardGame(unittest.TestCase):
     def test_should_not_generate_obstacles_on_player_and_finish_positions(self):
         self.assertTrue((self.board_game.player_position not in self.board_game.obstacles) and (self.board_game.finish not in self.board_game.obstacles))
 
+    def test_should_update_board_state_properly(self):
+        self.board_game.update_board_state()
+        self.assertEqual(self.board_game.board[self.board_game.finish[0]][self.board_game.finish[1]], 'F')
+        self.assertEqual(self.board_game.board[self.board_game.player_position[0]][self.board_game.player_position[1]], '*')
+        for obstacle in self.board_game.obstacles:
+            self.assertEqual(self.board_game.board[obstacle[0]][obstacle[1]], 'X')
+
+    def test_should_not_move_player_on_invalid_move_command(self):
+        initial_position = self.board_game.player_position
+        self.board_game.move('test')
+        self.assertEqual(self.board_game.player_position, initial_position)
+
+    def test_should_indicate_move_is_invalid(self):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.board_game.move('test')
+            printed_output = mock_stdout.getvalue().strip()
+            self.assertIn("Invalid move", printed_output)
+            
 
 if __name__ == '__main__':
     unittest.main()
